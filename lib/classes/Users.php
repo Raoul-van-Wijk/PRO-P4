03778b94076cc20baa3bb47f44601495082c4b85
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-05-11 15:51:11 +0200

    Changed url in headers

--- lib/classes/Users.php
+++ lib/classes/Users.php
@@ -12,7 +12,7 @@ class Users extends Config {
   function registerNewUser($username, $password, $age) {  
     if(empty($username) || empty($password)) {
       return "One or more fields are empty";
-      header("Refresh: 2; ".URLROOT."page/{$_GET['page']}&lr=register");
+      header("Refresh: 2; ".URLROOT."page/lr/register");
     } else {
       $sql = "INSERT INTO `users` (`userID`, `username`, `password`, `age`) VALUES (NULL, :username, :password, :age)";
       $stmt = $this->connect()->prepare($sql);
@@ -29,19 +29,19 @@ class Users extends Config {
       } else {
         return "An error occured";
       }
-      header("Refresh: 3; ".URLROOT."page/home");
+      header("Refresh: 3; ".URLROOT."page/lr/login");
     }
   }
 
   public function loginUsers($username, $password) {
     if(isset($_SESSION['userID'])) {
       return "Already logged in";
-      header("Refresh: 2; ".URLROOT."page/home");
+      header("Refresh: 2; ".URLROOT."page/main/dashboard");
       return false;
     }
     if(empty($username) || empty($password)) {
       return "One or more fields are empty";
-      header("Refresh: 2; ".URLROOT."page/{$_GET['page']}?lr=register");
+      header("Refresh: 2; ".URLROOT."page/lr/register");
     } else {
       $sql = "SELECT * FROM `users` WHERE `username` = :username";
       $stmt = $this->connect()->prepare($sql);
@@ -50,12 +50,12 @@ class Users extends Config {
       $count = $stmt->rowCount();
       if($count == 0) {
         return "User does not exist";
-        header("Refresh: 2; ".URLROOT."page/{$_GET['page']}?lr=register");
+        header("Refresh: 2; ".URLROOT."page/lr/register");
       } else {
         $loginCredentials = $stmt->fetch();
         if(!password_verify($password, $loginCredentials['password'])) {
           return "Password does not match";
-          header("Refresh: 2; ".URLROOT."page/{$_GET['page']}?lr=register");
+          header("Refresh: 2; ".URLROOT."page/lr/login");
         } else {
           if($loginCredentials['firstLogin'] == 1) {
             $mkNewProfile = new ProfileCustomization($loginCredentials['userID']);
@@ -64,7 +64,7 @@ class Users extends Config {
           return 'You succesfully logged in';
           $_SESSION['userID'] = $loginCredentials['userID'];
           $_SESSION['username'] = $username;
-          header("Refresh: 2; ".URLROOT."page/home");
+          header("Refresh: 2; ".URLROOT."page/main/dashboard");
         }
       }
     }
@@ -73,6 +73,6 @@ class Users extends Config {
   public function logoutUser() {
     session_destroy();
     return "succesfully logged out";
-    header("Refresh: 1; ". URLROOT ."page/home");
+    header("Refresh: 1; ". URLROOT ."page/lr/login");
   }
 }
