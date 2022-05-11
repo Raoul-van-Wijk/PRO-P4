4b1108bdb9893953c538bd45a2a31290c2fbb746
Sil333033 <94360907+Sil333033@users.noreply.github.com>
2022-05-11 15:56:15 +0200

    navbar beetje beter gedaan

--- views/molecules/navbar.php
+++ views/molecules/navbar.php
@@ -1,22 +1,7 @@
 <nav>
   <ul>
-    <li><a href="<?php echo URLROOT;?>page/main/home">home</a></li>
-    <li><a href="<?php echo URLROOT;?>page/main/chat">Chat</a></li>
-
-    <?php
-    if(isset($_SESSION['userID'])) {
-      echo '<li><a href="index.php?logout=true">Logout</a></li>';
-    } else {
-      echo "    
-        <li><a href='".URLROOT."page/lr/login'>login</a></li>
-        <li><a href='".URLROOT."page/lr/register'>Register</a></li>";
-    }
-    ?>
+    <!-- <li><a href="<?php echo URLROOT;?>page/main/home">home</a></li>
+    <li><a href="<?php echo URLROOT;?>page/main/chat">Chat</a></li> -->
+    <li><a>username</a></li>
   </ul>
 </nav>
-<?php
-  if(isset($_GET['logout'])) {
-    $logout = new Users;
-    $logout->logoutUser();
-  }
-?>
