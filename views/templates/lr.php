8cb21e1115bd598a263d1407b7186a8fa84b6773
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-05-17 14:08:26 +0200

    Added templates

---
+++ views/templates/lr.php
@@ -0,0 +1,3 @@
+<main>
+    <?php loadContent() ?>
+  </main>
---
+++ views/templates/main.php
@@ -0,0 +1,5 @@
+<header><?php require_once('./views/molecules/navbar.php'); ?></header>
+  <main>
+    <?php loadContent() ?>
+  </main>
+<footer><?php require_once('./views/molecules/footer.php'); ?></footer>
