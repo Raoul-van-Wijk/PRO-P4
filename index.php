37258136ff72aa0c445de5b394b57335e04e3bfc
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-05-17 14:06:46 +0200

    Added scripts tag to include ajax.js

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   session_start(); session_gc();
-  require('lib/index.php');
+  require_once('lib/index.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +12,9 @@
   <link rel="stylesheet" href="<?php echo URLROOT ?>assets/styles/css/style.css">
   <!-- link javascript -->
   <script src="<?php echo URLROOT ?>assets/js/app.js"></script>
+  <script src="<?php echo URLROOT ?>assets/js/ajax.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>  
+  <link rel="stylesheet" href="<?= $_SERVER['DOCUMENT_ROOT'] ?>/views/molecules/test.css">
 </head>
 <body>
   <?php loadTemplate(); ?>
