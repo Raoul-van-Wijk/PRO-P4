ffb2903de53dd6d1083083de69ec98e2ec024d47
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-05-17 15:20:07 +0200

    Change: if user not logged in redirect

--- views/templates/main.php
+++ views/templates/main.php
@@ -1,3 +1,9 @@
+<?php
+  if(!isset($_SESSION['userID'])) {
+    header("location: ". URLROOT ."page/lr");
+  }
+?>
+
 <header><?php require_once('./views/molecules/navbar.php'); ?></header>
   <main>
     <?php loadContent() ?>
