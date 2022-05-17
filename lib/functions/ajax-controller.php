473ae30763e70af5c7be5768d93ed9e57ca4bf58
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-05-17 14:09:23 +0200

    changed some post variables names

--- lib/functions/ajax-controller.php
+++ lib/functions/ajax-controller.php
@@ -1,14 +1,17 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/classes/Chat.php';
+$chat = new Chat(1);
 
 if (!isset($_POST['func'])) { return false;}
 if (!isset($_SESSION['userID'])) { return false;}
 $chat = new Chat($_SESSION['userID']);
+
 switch ($_POST['func']) {
-  case 'send-msg':
-    $chat->sendMsg($_POST['toUser'], $_POST['message']);
+  case 'send-msg': 
+    $chat->sendMsg($_POST['toUser'], $_POST['msg']);
     break;
   case 'get-msg':
-    $chat->getMsg($_POST['toUser']);
+    $chat->getMsg($_POST['fromUser']);
     break;
   case 'commentUser':
     $chat->commentUser($_POST['toUser']);
