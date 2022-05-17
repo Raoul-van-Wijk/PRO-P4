b73060d8aae6a019ea6c3512765460169e2af152
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-05-17 14:06:32 +0200

    Added chat class

--- lib/classes/Chat.php
+++ lib/classes/Chat.php
@@ -1,5 +1,21 @@
 <?php
-
+require_once '1Config.php';
+// Class for chat
   class Chat extends Config {
-    
+    private int $userID;
+    public function __Construct(int $userID) {
+      $this->userID = $userID;
+    }
+
+    public function sendMsg($toUser) {
+
+    }
+
+    public function getMsg($toUser) {
+      
+    }
+
+    public function commentUser($toUser) {
+
+    }
   }
