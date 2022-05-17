eb97c0874b5ac32fede68ac806ade11234d6812b
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-05-17 14:07:47 +0200

    Changed: location to server doc root so it works everywhere

--- lib/classes/Files.php
+++ lib/classes/Files.php
@@ -2,24 +2,36 @@
 
 class Files 
 {
+
   private string $username;
-  private string $DIRNAME = "E:\Project/PRO-P4/assets/img/users-img/";
-  public function __CONSTRUCT($username) {
+  private string $DIRNAME =   "/assets/img/users-img/";
+  
+  public function __CONSTRUCT($username = '') {
     $this->username = $username;
   }
   public function makeDir() {
-    if(!is_dir($this->DIRNAME. $this->username)) {
-      mkdir($this->DIRNAME. $this->username);
-      mkdir($this->DIRNAME. $this->username. '/pfp');
-      return $this->DIRNAME. $this->username;
+    if(!is_dir($_SERVER['DOCUMENT_ROOT'] . $this->DIRNAME. $this->username)) {
+      mkdir($_SERVER['DOCUMENT_ROOT'] . $this->DIRNAME. $this->username);
+      mkdir($_SERVER['DOCUMENT_ROOT'] . $this->DIRNAME. $this->username. '/pfp');
+      return $_SERVER['DOCUMENT_ROOT'] . $this->DIRNAME. $this->username;
     }
   }
+  
   public function remDir() {
     if(is_dir($this->DIRNAME. $this->username)) {
       rmdir($this->DIRNAME. $this->username);
     }
   }
   public function renameImage($img) {
-    
+    move_uploaded_file($tmpname, 'test/'. $newname);
   }
 }
+
+
+
+
+
+// if (isset($_POST['submit'])) {
+//   $PFC = new ProfileCustomization($_SESSION['userID'], $_FILES);
+//   $PFC->updateProfilePicture();
+// }
