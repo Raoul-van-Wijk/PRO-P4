3efa2e1d184b5af9f5301e1c2072b02880bdf6c1
Sil333033 <94360907+Sil333033@users.noreply.github.com>
2022-05-18 16:28:49 +0200

    begin met dashboard

--- views/organisms/dashboard.php
+++ views/organisms/dashboard.php
@@ -1,1 +1,23 @@
-admin dashboard
+<?php 
+    $users = new Users();
+    $rows = $users->getUsers();
+
+    $records = "";
+
+    foreach($rows as $row) {
+        $records .= "<tr><td>"
+        . $row["userID"] . "</td><td>"
+        . $row["username"] . "</td><td>"
+        . $row["age"] . "</td><td>"
+        . $row["userrole"] . "</td><td>"
+        . $row["firstLogin"] . "</td></tr>";
+    }
+?>
+
+<div class="dashboard">
+<table>
+    <tbody>
+    <?php echo $records; ?>
+    </tbody>
+</table>
+</div>
