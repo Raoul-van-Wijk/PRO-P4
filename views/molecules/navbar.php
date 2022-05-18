133fa451a09aad22261bc3d51f95f0c368dca716
Sil333033 <94360907+Sil333033@users.noreply.github.com>
2022-05-18 16:29:01 +0200

    Update navbar.php

--- views/molecules/navbar.php
+++ views/molecules/navbar.php
@@ -19,7 +19,7 @@
         <a href="">my profile</a>
         <?php 
           if($_SESSION['userrole'] == 'admin' || $_SESSION['userrole'] ==  'root') {
-            echo '<a href="">admin dashboard</a>';
+            echo '<a href="'.  URLROOT . 'page/main/dashboard">admin dashboard</a>';
           }
         ?>
         <a href="&logout=true">logout</a>
