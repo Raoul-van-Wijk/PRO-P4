be66f715cd948f66ac8451a1dc98ef7c3276627d
Sil333033 <94360907+Sil333033@users.noreply.github.com>
2022-05-31 14:07:31 +0200

    class User aangepast
    Hierdoor werd het aangepast zodat de roles goed werken

--- lib/classes/Users.php
+++ lib/classes/Users.php
@@ -4,7 +4,7 @@ class Users extends Config {
   public function getUsers() {
     $sql = "SELECT * FROM users";
     $stmt = $this->connect()->query($sql);
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $rows = $stmt->fetchAll();
     return $rows;
   }
 
@@ -58,8 +58,12 @@ class Users extends Config {
           return "Password does not match";
         } else {
           if($loginCredentials['firstLogin'] == 1) {
+            var_dump($loginCredentials);exit;
             $mkNewProfile = new ProfileCustomization($loginCredentials['userID']);
             $mkNewProfile->addUserProfile();
+          } elseif ($loginCredentials['firstLogin'] == 2 || $loginCredentials['firstLogin'] == 3) {
+            header("location: ".URLROOT."page/ban");
+            exit();
           }
           $_SESSION['userID'] = $loginCredentials['userID'];
           $_SESSION['username'] = $username;
@@ -75,5 +79,49 @@ class Users extends Config {
     session_destroy();
     header("Refresh: 1; ". URLROOT ."page/lr/login");
     return "succesfully logged out";
+  }
+
+  public function deleteUser($id) {
+    $sql = "DELETE FROM `users` where `userID` = :id";
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    header("Refresh: 1; ". URLROOT ."page/main/dashboard");
+    return "succesfully removed user";
+  }
+
+  public function getUserByID($id) {
+    $sql = "SELECT * FROM users WHERE userID = :id";
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $row = $stmt->fetchAll();
+    return $row;
+  }
+
+  public function timeoutUser($id) {
+    $status = self::userStatus($id);
+
+    $sql = '';
+
+    if($status[0]['firstLogin'] == 0) {
+      $sql = "UPDATE `users` SET `firstLogin` = 2 where `userID` = :id";
+    } elseif ($status[0]['firstLogin'] == 2) {
+      $sql = "UPDATE `users` SET `firstLogin` = 0 where `userID` = :id";
+    }
+
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    header("Refresh: 1; ". URLROOT ."page/main/dashboard");
+    return "succesfully timed-out user";
+  }
+
+  public function userStatus($id) {
+    $sql = "SELECT `firstLogin` FROM `users` where `userID` = :id";
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    return $stmt->fetchAll();
   }
 }
