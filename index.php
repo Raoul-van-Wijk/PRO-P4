8803cc579b08bb579372e70116274bc8ed742d88
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-05-31 14:08:30 +0200

    Change: added checkpage function to body

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
   <script defer src="<?php echo URLROOT ?>assets/js/app.js"></script>
   <script defer src="<?php echo URLROOT ?>assets/js/ajax.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>  
-  <link rel="stylesheet" href="<?= $_SERVER['DOCUMENT_ROOT'] ?>/views/molecules/test.css">
+  <link rel="stylesheet" href="<?= URLROOT ?>views/atoms/test.css">
 </head>
-<body class="<?php echo checkPage() ?>">
+<body class="<?php echo checkPage('index') ?>">
   <?php loadTemplate(); ?>
 </body>
 </html>
