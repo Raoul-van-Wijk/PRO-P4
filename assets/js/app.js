7db569f076902730c622e173569d7473e98d0848
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-05-31 20:19:30 +0200

    CHange: added the selector for edit profile button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
 document.querySelector('[data-open-aside]').addEventListener('click', () => {
   document.querySelector('main').classList.toggle('active-chat');
 })
+
+document.querySelector('[data-edit-profile-btn]').addEventListener('click', () => {
+  document.querySelector('[data-edit-profile]').classList.toggle('active');
+})
