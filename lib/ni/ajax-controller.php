8ab9a1f4eba5c1f8c89608d602a96e71945a1725
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-05-31 14:08:47 +0200

    Change: updated the ajax controller

--- lib/ni/ajax-controller.php
+++ lib/ni/ajax-controller.php
@@ -6,24 +6,36 @@ $chat = new Chat();
 
 switch ($_POST['func']) {
   case 'send-msg': 
-    $chat->sendMsg($_POST['toUser'], $_POST['msg'], $_POST['userID']);
+    if(!empty($_POST['msg'])) {
+      $chat->sendMsg($_POST['toUser'], $_POST['msg'], $_POST['userID']);
+    }
+    
     break;
 
   case 'get-msg':
+    if($_POST['fromUser'] == 'NaN') {
+      echo $_POST['fromUser'];exit;
+    }
     $user = $chat->getUser($_POST['fromUser']);
-    echo "<div class='chat-header'>
-            <img src='' alt=''>
-            <p data-toUser='{$user['userID']}'>{$user['username']}</p>
-          </div>";
+    // echo "<div class='chat-header'>
+    //         <img src='../../assets/img/logo.PNG' alt='Profile picture'>
+    //         <p data-toUser='{$user['userID']}'>{$user['username']}</p>
+    //       </div>
+    //       ";
     $msg = $chat->getMsg($_POST['UserID'], $_POST['fromUser']);
+    echo "<div class='msg'>";
     while ($row = $msg->fetch()) {
       if($row['FromUserID'] == $_POST['UserID']) {
-        echo "<div class='fromUser'>{$row['Message']}</div>";
+        echo "<div class='send'>
+                <p>{$row['Message']}</p>
+              </div>";
       } else {
-        echo "<div class='toUser'>{$row['Message']}</div>";
+        echo "<div class='receive'>
+        <p>{$row['Message']}</p>
+      </div>";
       }
     }
-
+    echo "</div>";
     break;
 
   case 'commentUser':
@@ -34,3 +46,17 @@ switch ($_POST['func']) {
     return false;
     break;
 }
+
+// {$user['pfp']}
+
+// <div class="send">
+//   <p>i send a message</p>
+//   </div>
+//   <div class="receive">
+//   <p>you got a message asfljs lkdjf lksjdf lkdafj lkdf dkd kdf kskdfkjd fj dfkkf dfk dkfjdfjdf df salls sldfk
+//     slkf</p>
+//   </div>
+//   </div>
+//   <div class="send-message">
+//   <input data-message-input type="text" placeholder="type here...">
+//   <button data-message-button>send</button>
