b5f39bbd2c44eed56dfa8e24f5e5ea457226de7e
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-06-01 14:55:27 +0200

    CHange: fixed a bug

--- views/organisms/register.php
+++ views/organisms/register.php
@@ -2,10 +2,10 @@
   <input type="text" name="username">
   <input type="password" name="password">
   <input type="text" name="age" placeholder="Age">
-  <button type="submit" name="submit">Register</button>
+  <button type="submit" name="submit-r">Register</button>
 </form>
 <?php
-  if(isset($_POST['submit'])) {
+  if(isset($_POST['submit-r'])) {
     $username = sanitize($_POST['username']);
     $password = sanitize($_POST['password']);
     $age = sanitize($_POST['age']);
