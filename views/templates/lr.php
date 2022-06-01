f919052cfbaf979df61195abc50e74d57e44d96a
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-06-01 14:55:36 +0200

    Change: added some html

--- views/templates/lr.php
+++ views/templates/lr.php
@@ -1,6 +1,6 @@
   <main class="lr">
-    <div class="container">
-      <div class="left"><h1>test</h1></div>
+    <div class="container-lr">
+      <div class="left"><?php include 'views/molecules/registration.php' ?></div>
       <div class="right"><?php include 'views/atoms/test.php' ?></div>
     </div>
     
