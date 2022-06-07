e43200242dd23d23a2e4552f68306dff9766bd32
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-06-07 12:42:46 +0200

    Change: Added styling for user dasboard

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,24 @@ document.querySelector('[data-open-aside]').addEventListener('click', () => {
 })
 }
 
-const ProfileButton = document.querySelector('[data-edit-profile-btn]')
-ProfileButton.addEventListener('click', () => {
-  document.querySelector('[data-edit-profile]').classList.toggle('active');
-})
+
+
+
+document.querySelectorAll('[data-popup-open]').forEach(button => {
+  button.addEventListener('click', event => {
+    const target = button.getAttribute('data-popup-open');
+    if (!target) return;
+    const element = document.querySelector(`[data-popup="${target}"]`);
+    element.showModal();
+  });
+});
+
+document.querySelectorAll('[data-popup-close]').forEach(button => {
+  button.addEventListener('click', event => {
+    event.preventDefault();
+    const target = button.getAttribute('data-popup-close');
+    if (!target) return;
+    const element = document.querySelector(`[data-popup="${target}"]`);
+    element.close();
+  });
+});
